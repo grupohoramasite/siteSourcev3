86fdd88dbaa013481d2b677ae7ef012aaea34b73
Ezequiel Tejada <et.github@outlook.com>
2021-02-21 16:32:37 +0100

    feat: Add i18n and Home Hero

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeLayoutComponent } from './components/home-layout/home-layout.component';
 import { MainHeroComponent } from './components/main-hero/main-hero.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../core/core.module';
+import { HttpClient } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [HomeLayoutComponent, MainHeroComponent],
   imports: [
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     HomeRoutingModule
   ]
 })
