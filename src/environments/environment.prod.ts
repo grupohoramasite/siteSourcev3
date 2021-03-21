86f99f8855ee5df4f8e8a8225b393f261213d760
Ezequiel Tejada <et.github@outlook.com>
2021-03-21 13:14:30 +0100

    fix: :ambulance: Add contactForm variables for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contactForm: {
+    actionUrl: 'https://api.staticforms.xyz/submit',
+    accessKey: '45bf5230-88e3-46a9-b858-20b5a4c2dc29'
+  }
 };
