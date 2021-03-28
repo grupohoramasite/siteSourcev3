53169ddaff9a33b6cd2917b0a87083f24f6e5e42
Ezequiel Tejada <et.github@outlook.com>
2021-03-28 11:01:45 +0200

    feat(Services): :sparkles: Finalize services styling

--- src/app/services/components/services/services.component.ts
+++ src/app/services/components/services/services.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ServicesComponent implements OnInit {
+  services = [
+    {
+      title: 'services.securityaudit.title',
+      description: 'services.securityaudit.description',
+      imgsrc: '/assets/services/security@2x.png'
+    },
+    {
+      title: 'services.implementation.title',
+      description: 'services.implementation.description',
+      imgsrc: '/assets/services/implementacion@2x.png'
+    },
+    {
+      title: 'services.racadministration.title',
+      description: 'services.racadministration.description',
+      imgsrc: '/assets/services/oracleRAC@2x.png'
+    },
+    {
+      title: 'services.standaloneadministration.title',
+      description: 'services.standaloneadministration.description',
+      imgsrc: '/assets/services/man-working-1200@2x.png'
+    },
+  ];
 
   constructor() { }
 
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './components/services/services.component';
+import { CoreModule } from '../core/core.module';
 
 
 @NgModule({
   declarations: [ServicesComponent],
   imports: [
     CommonModule,
+    CoreModule,
     ServicesRoutingModule
   ]
 })
