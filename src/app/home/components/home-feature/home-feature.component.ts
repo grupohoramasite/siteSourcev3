9befef3f694922be4080f228f7ef32634f9b0b15
Ezequiel Tejada <et.github@outlook.com>
2021-12-14 16:24:30 +0100

    fix: Move home features to nginit

--- src/app/home/components/home-feature/home-feature.component.ts
+++ src/app/home/components/home-feature/home-feature.component.ts
@@ -8,38 +8,39 @@ import { TranslateService } from '@ngx-translate/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeFeatureComponent implements OnInit {
-  features = [
-    {
-      title: this.translateService.instant('home.feature.backup.title'),
-      description: this.translateService.instant('home.feature.backup.description'),
-      image: {
-        src: 'assets/styles/SVG/backup-icon.svg',
-        alt: this.translateService.instant('home.feature.backup.title')
-      }
-    },
-    {
-      title: this.translateService.instant('home.feature.identity.title'),
-      description: this.translateService.instant('home.feature.identity.description'),
-      image: {
-        src: 'assets/styles/SVG/identity-icon.svg',
-        alt: this.translateService.instant('home.feature.identity.title')
-      }
-    },
-    {
-      title: this.translateService.instant('home.feature.high-availability.title'),
-      description: this.translateService.instant('home.feature.high-availability.description'),
-      image: {
-        src: 'assets/styles/SVG/high-availability-icon.svg',
-        alt: this.translateService.instant('home.feature.high-availability.title')
-      }
-    },
-  ];
+  features = [  ];
 
   constructor(
     private translateService: TranslateService
   ) { }
 
   ngOnInit(): void {
+    this.features = [
+      {
+        title: this.translateService.instant('home.feature.backup.title'),
+        description: this.translateService.instant('home.feature.backup.description'),
+        image: {
+          src: 'assets/styles/SVG/backup-icon.svg',
+          alt: this.translateService.instant('home.feature.backup.title')
+        }
+      },
+      {
+        title: this.translateService.instant('home.feature.identity.title'),
+        description: this.translateService.instant('home.feature.identity.description'),
+        image: {
+          src: 'assets/styles/SVG/identity-icon.svg',
+          alt: this.translateService.instant('home.feature.identity.title')
+        }
+      },
+      {
+        title: this.translateService.instant('home.feature.high-availability.title'),
+        description: this.translateService.instant('home.feature.high-availability.description'),
+        image: {
+          src: 'assets/styles/SVG/high-availability-icon.svg',
+          alt: this.translateService.instant('home.feature.high-availability.title')
+        }
+      },
+    ];
   }
 
 }
